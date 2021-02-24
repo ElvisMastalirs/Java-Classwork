bd0a973578022715daa4c51de36504ef5e1ba361
ElvisMastalirs <mastalirs.elvis@gmail.com>
2021-02-24 20:09:48 +0200

    commited

--- src/com/rcs/classwork/Day5/CheckPassFail.java
+++ src/com/rcs/classwork/Day5/CheckPassFail.java
@@ -4,14 +4,13 @@ import java.util.Scanner;
 public class CheckPassFail {
 
     public static void main(String[] args) {
-	    Scanner scanner = new Scanner(System.in);
-	    System.out.println("Enter grade: ");
-	    int grade = scanner.nextInt();
-	    scanner.close();
-
-	    if(grade >= 50) {
+		Scanner scanner = new Scanner(System.in);
+		System.out.println("Enter grade: ");
+		int grade = scanner.nextInt();
+		scanner.close();
+		  if(grade >= 50) {
 	        System.out.println("Pass");
-        } else {
+		  } else {
 	        System.out.println("Fail");
         }
             System.out.println("Done");
--- src/com/rcs/classwork/Day5/OddOrEven.java
+++ src/com/rcs/classwork/Day5/OddOrEven.java
@@ -4,13 +4,12 @@ import java.util.Scanner;
 
 public class OddOrEven {
 
-    public static void main(String[] args){
+    public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
         System.out.println("Ieraksti skaitli: ");
         int number = scanner.nextInt();
         scanner.close();
-
-        if(number % 2 == 0){
+        if (number % 2 == 0) {
             System.out.println("Skaitlis ko ievadīji ir pāra skaitlis");
         } else {
             System.out.println("Skaitlis ko ievadīji ir nepāra skaitlis");
