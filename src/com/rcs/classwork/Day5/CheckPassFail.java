1bd32b965763d1b07b7b031546b140a9cb777056
ElvisMastalirs <mastalirs.elvis@gmail.com>
2021-02-24 19:44:41 +0200

    classwork

--- src/com/rcs/classwork/Day5/CheckPassFail.java
+++ src/com/rcs/classwork/Day5/CheckPassFail.java
@@ -1,8 +1,19 @@
 package com.rcs.classwork.Day5;
+import java.util.Scanner;
 
 public class CheckPassFail {
 
     public static void main(String[] args) {
-	// write your code here
+	    Scanner scanner = new Scanner(System.in);
+	    System.out.println("Enter grade: ");
+	    int grade = scanner.nextInt();
+	    scanner.close();
+
+	    if(grade >= 50) {
+	        System.out.println("Pass");
+        } else {
+	        System.out.println("Fail");
+        }
+            System.out.println("Done");
     }
 }
