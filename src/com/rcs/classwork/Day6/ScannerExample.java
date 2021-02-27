d9b5ae21fad0f840a34b3e2ff873401b0eecb5f1
ElvisMastalirs <mastalirs.elvis@gmail.com>
2021-02-27 14:18:06 +0200

    punkti

---
+++ src/com/rcs/classwork/Day6/ScannerExample.java
@@ -0,0 +1,29 @@
+package com.rcs.classwork.Day6;
+
+import java.util.Scanner;
+
+public class ScannerExample {
+    public static void main(String[] args) {
+        int x = 0;
+        Scanner scanner = new Scanner(System.in);
+        System.out.print("Ievadi skaitli: ");
+        if (scanner.hasNextDouble()) {
+            System.out.println(scanner.nextDouble());
+            x++;
+        }
+        scanner.nextLine();
+        System.out.print("Ievadi boolean tipa vērtību: ");
+        if (scanner.hasNextBoolean()){
+            System.out.println(scanner.nextBoolean());
+            x++;
+        }
+        scanner.nextLine();
+        System.out.print("Ievadi tekstu: ");
+        if (scanner.hasNextLine()){
+            System.out.println(scanner.nextLine());
+            x++;
+        }
+        System.out.println("Puntku skaits: " + x);
+
+    }
+}
